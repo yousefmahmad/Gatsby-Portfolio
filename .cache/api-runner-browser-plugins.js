1e284c3979f9fa5f81534310bbfec508ea4d2eb8
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-04 13:07:56 -0400

    chess

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('/Users/yousefahmad/sei32/projects/Gatsby-Portfolio/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/images/website-icon.png"},
+    },{
+      plugin: require('/Users/yousefahmad/sei32/projects/Gatsby-Portfolio/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/yousefahmad/sei32/projects/Gatsby-Portfolio/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/yousefahmad/sei32/projects/Gatsby-Portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/yousefahmad/sei32/projects/Gatsby-Portfolio/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/yousefahmad/sei32/projects/Gatsby-Portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
